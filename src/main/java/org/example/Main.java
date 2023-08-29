0f704dd446d9e858fa0f651008730e73ac2312fc
Rashid K <2011rashid.k@gmail.com>
2023-08-29 18:47:13 +0530

    changed the code to get IP and host

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,18 @@ package org.example;
 import java.util.List;
 
 public class Main {
+
+    public static final String SFTP_USERNAME = "sftpappuser";
+    public static final String SFTP_PASSWORD = "Test@123";
+    public static final String SFTP_DIRECTORY = "/C:/Users/sftpappuser/EmployeeRecord";
+    public static final String FILE_NAME = "HappiestMinds.json";
+
     public static void main(String[] args) {
         SFTPUtil sftpUtil = new SFTPUtil();
-        String fileContent = sftpUtil.readFileFromSftp("10.16.32.225", 22, "sftpappuser", "Test@123",
-                "/C:/Users/sftpappuser/EmployeeRecord", "HappiestMinds.json");
+        String fileContent = sftpUtil.readFileFromSftp(SFTP_USERNAME, SFTP_PASSWORD, SFTP_DIRECTORY, FILE_NAME);
         System.out.println("fileContent: " + fileContent);
-        List<String> listOfFiles = sftpUtil.getAllFilesInDir("10.16.32.225", 22, "sftpappuser", "Test@123",
-                "/C:/Users/sftpappuser/EmployeeRecord");
+        List<String> listOfFiles = sftpUtil.getAllFilesInDir(SFTP_USERNAME, SFTP_PASSWORD, SFTP_DIRECTORY);
         System.out.println("listOfFiles = " + listOfFiles);
     }
+
 }
--- src/main/java/org/example/SFTPUtil.java
+++ src/main/java/org/example/SFTPUtil.java
@@ -3,16 +3,24 @@ package org.example;
 import com.jcraft.jsch.*;
 
 import java.io.ByteArrayOutputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
 public class SFTPUtil {
 
-    public String readFileFromSftp(String sftpHost, int sftpPort, String sftpUsername, String sftpPassword, String sftpDirectory, String fileName) {
+    public static final int PORT = 22;
+
+    public void test() {
+        System.out.println("Hello from SFTP library");
+    }
+
+    public String readFileFromSftp(String sftpUsername, String sftpPassword, String sftpDirectory, String fileName) {
         try {
             JSch jsch = new JSch();
-            Session session = jsch.getSession(sftpUsername, sftpHost, sftpPort);
+            Session session = jsch.getSession(sftpUsername, InetAddress.getLocalHost().getHostAddress(), PORT);
             ChannelSftp channel = connectSftpServer(sftpPassword, session);
             channel.cd(sftpDirectory);
             channel.get(fileName);
@@ -22,15 +30,15 @@ public class SFTPUtil {
             channel.disconnect();
             session.disconnect();
             return fileContent;
-        } catch (JSchException | SftpException e) {
+        } catch (JSchException | SftpException | UnknownHostException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public List<String> getAllFilesInDir(String sftpHost, int sftpPort, String sftpUsername, String sftpPassword, String sftpDirectory) {
+    public List<String> getAllFilesInDir(String sftpUsername, String sftpPassword, String sftpDirectory) {
         try {
             JSch jsch = new JSch();
-            Session session = jsch.getSession(sftpUsername, sftpHost, sftpPort);
+            Session session = jsch.getSession(sftpUsername, InetAddress.getLocalHost().getHostAddress(), PORT);
             ChannelSftp channel = connectSftpServer(sftpPassword, session);
             Vector<ChannelSftp.LsEntry> listOfFiles = channel.ls(sftpDirectory);
             List<String> files = new ArrayList<>();
@@ -38,7 +46,7 @@ public class SFTPUtil {
                 files.add(lsEntry.getFilename());
             }
             return files;
-        } catch (JSchException | SftpException e) {
+        } catch (JSchException | SftpException | UnknownHostException e) {
             throw new RuntimeException(e);
         }
     }
